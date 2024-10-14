42c35542d9d78fa503caa45b49c11c0e35411785
TGUY-8 <luxiran816@gmail.com>
2024-10-14 10:50:44 +0800

    Update struct2.c

--- struct2.c
+++ struct2.c
@@ -6,32 +6,32 @@ struct stu
     char name[10];
     int id[8];
     int age;
-}
-typedf struct stu stu;
+};
+typedef struct stu stu;
 stu info[55];
-void swap(*x,*y)
+void swap(int *x,int *y)
 {
     int t;
-    t=*x;
-    *x=*y;
-    *y=t;
+    t = *x;
+    *x = *y;
+    *y = t;
 }
 int main()
 {
-    int n,m;
-    scanf("%d %d",&n,&m);
-    for(int i=0;i<n;i++)
+    int n, m;
+    scanf_s("%d %d", &n, &m);
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d%s%d%d",&info[i].serial,&info[i].name,&info[i].id,&info[i].age);
+        scanf_s("%d %s %d %d", &info[i].serial, &info[i].name, &info[i].id, &info[i].age);
     }
-    while(m--)
+    while (m--)
     {
-        int a,b;
-        scanf("%d %d",&a,&b);
-        swap(&info[a].serial,&info[b].serial);
-        swap(&info[a].name,&info[b].name);
-        swap(&info[a].id,&info[b].id);
-        swap(&info[a].age,&info[b].age);
+        int a, b;
+        scanf_s("%d %d", &a, &b);
+        swap(&info[a].serial, &info[b].serial);
+        swap(&info[a].name, &info[b].name);
+        swap(&info[a].id, &info[b].id);
+        swap(&info[a].age, &info[b].age);
     }
     return 0;
 }
